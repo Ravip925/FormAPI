8e66e0df578376c593a9b786d63c4482f4873088
“Ravi <patilravi400@gmail.com>
2023-03-30 22:56:14 +0530

    api created

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+const mongoose = require("mongoose");
+const formRoute = require("./routes/formRoute");
+const app = express();
+app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({
+  extended: true
+}));
+
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },)
+  .then(() => {
+    console.log("db connection successfull");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.get("/api/data", (req, res) => {
+  res.send("successfull");
+})
+
+app.use("/api/form", formRoute);
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("server is running");
+});
