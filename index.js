cab52bea69fee2a6ac5c2905fdf501f1cc347478
“Ravi <patilravi400@gmail.com>
2023-03-31 10:07:20 +0530

    api created

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
 const mongoose = require("mongoose");
-const formRoute = require("./routes/formRoute");
+const formDataRoute = require('./routes/formRoute')
 const app = express();
 app.use(cors());
 app.use(express.json())
@@ -28,7 +28,7 @@ app.get("/api/data", (req, res) => {
   res.send("successfull");
 })
 
-app.use("/api/form", formRoute);
+app.use("/api/form", formDataRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("server is running");
